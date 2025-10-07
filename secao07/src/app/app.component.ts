e5b30982add1f648efe5c18e6c75f647bcfc6fad
PattyPC <147942494+Pattypc@users.noreply.github.com>
2025-10-07 07:28:09 -0300

    just save

---
+++ secao07/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'secao07';
+  teste = 'valor inicial';
+}
